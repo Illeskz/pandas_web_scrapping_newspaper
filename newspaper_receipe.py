938a976d029e62115624d38adeb16f7bc8e6bf9f
AngelIllescas <angelillescasmalagon@gmail.com>
2019-10-09 21:50:36 -0500

    The NaN registers were removed form the df and include the Title of the article in the Title column

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -9,19 +9,23 @@ import pandas as pd
 
 logger = logging.getLogger(__name__)  #esto es solo para saber donde se está ejecutando nuestro codigo.
 
+
 def main(filename):
     logger.info('Starting cleaning process')
     df = _read_data(filename)
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
 
     return df
+
 
 def _read_data(filename):
     logger.info('Reading file {}'.format(filename))
 
     return pd.read_csv(filename, encoding = 'ISO-8859-1')
+
 
 def _extract_newspaper_uid(filename):
     logger.info('Extarcting newspaper uid')
@@ -31,15 +35,31 @@ def _extract_newspaper_uid(filename):
 
     return newspaper_uid
 
+
 def _add_newspaper_uid_column(df, newspaper_uid):
     logger.info('Filling newspaper_uid column with {}'.format(newspaper_uid))
     df['newspaper_uid'] = newspaper_uid
 
     return df
 
+
 def _extract_host(df):
     logger.info('Extracting host from urls')
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
+
+    return df
+
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .applymap(lambda title: title.split('-'))
+                        .applymap(lambda title_word_list: ''.join(title_word_list))
+                    )
+    df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
 
     return df
 
@@ -49,7 +69,7 @@ if __name__ =='__main__':
 			help='The path to the dirty data',
 			type=str)
 
-    args = parser.parse_args() 
+    args = parser.parse_args()
 
     df = main(args.filename)
     print(df)

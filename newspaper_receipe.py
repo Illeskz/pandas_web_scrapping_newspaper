c7731ebd6876b17add4449e15b82c5645f569e0c
AngelIllescas <angelillescasmalagon@gmail.com>
2019-10-23 19:30:51 -0500

    Removing the duplicated rows and save data to csv

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -28,6 +28,9 @@ def main(filename):
     df = _remove_new_lines_from_body(df)
     df = _tokenize_column(df, 'title')
     df = _tokenize_column(df, 'body')
+    df = _remove_duplicate_entries(df, 'title')
+    df = _drop_rows_with_missing_values(df)
+    _save_data(df, filename)
 
     return df
 
@@ -118,6 +121,23 @@ def _tokenize_column(df, column_name):
 
     return df
 
+
+def _remove_duplicate_entries(df, column_name):
+    logger.info('Removing duplicate entries')
+    df.drop_duplicates(subset=[column_name], keep='first')
+
+    return df
+
+
+def _drop_rows_with_missing_values(df):
+    logger.info('Dropping rows with missing values')
+    return df.dropna()
+
+
+def _save_data(df, filename):
+    clean_filename = 'clean_{}'.format(filename)
+    logger.info('Saving data at location: {}'.format(clean_filename))
+    df.to_csv(clean_filename)
 
 if __name__ =='__main__':
     parser = argparse.ArgumentParser()
